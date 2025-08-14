0ee100e2ea51f64baf0f9e96f45408804885c664
Victorian <victorianbelayev@gmail.com>
2025-08-14 14:11:09 +0300

    גקגק

--- src/main/java/org/example/Level1.java
+++ src/main/java/org/example/Level1.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import javax.print.attribute.standard.OrientationRequested;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyAdapter;
@@ -9,27 +10,32 @@ public class Level1 extends GamePanel{
 
     public static final int BLOCK = 1;
     public static final int CARROT = 2;
+    public static final int EMPTY = 0;
 
     public static final int TILE_SIZE = 55;
     public static final int COLS = 18; //Main.WINDOW_WIDTH / TILE_SIZE;
     public static final int ROW = 14 ;//Main.WINDOW_HEIGHT / TILE_SIZE;
 
+    private int carrotCount = 0;
+    private int totalCarrot;
+
     private int[][] map = {
-            {1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0, 2, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
-            {0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+            {BLOCK,BLOCK,BLOCK,BLOCK,BLOCK,BLOCK,BLOCK,BLOCK,BLOCK,BLOCK,BLOCK,BLOCK,BLOCK,BLOCK,BLOCK,BLOCK,BLOCK,BLOCK},
+            {BLOCK,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,CARROT,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,BLOCK},
+            {BLOCK,EMPTY,BLOCK,BLOCK,BLOCK,EMPTY,BLOCK,BLOCK,BLOCK,EMPTY,BLOCK,BLOCK,BLOCK,EMPTY,BLOCK,BLOCK,EMPTY,BLOCK},
+            {BLOCK,EMPTY,BLOCK,CARROT,BLOCK,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,BLOCK,EMPTY,EMPTY,BLOCK,EMPTY,BLOCK},
+            {BLOCK,EMPTY,BLOCK,EMPTY,BLOCK,BLOCK,BLOCK,EMPTY,BLOCK,BLOCK,BLOCK,EMPTY,BLOCK,BLOCK,EMPTY,BLOCK,EMPTY,BLOCK},
+            {BLOCK,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,BLOCK},
+            {BLOCK,EMPTY,BLOCK,BLOCK,BLOCK,EMPTY,BLOCK,BLOCK,BLOCK,EMPTY,BLOCK,BLOCK,BLOCK,EMPTY,BLOCK,BLOCK,EMPTY,BLOCK},
+            {BLOCK,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,CARROT,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,BLOCK},
+            {BLOCK,EMPTY,BLOCK,EMPTY,BLOCK,BLOCK,EMPTY,BLOCK,BLOCK,BLOCK,EMPTY,BLOCK,BLOCK,EMPTY,BLOCK,EMPTY,BLOCK,BLOCK},
+            {BLOCK,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,CARROT,BLOCK},
+            {BLOCK,EMPTY,BLOCK,BLOCK,BLOCK,EMPTY,BLOCK,BLOCK,BLOCK,EMPTY,BLOCK,BLOCK,BLOCK,EMPTY,BLOCK,BLOCK,EMPTY,BLOCK},
+            {BLOCK,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,CARROT,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,BLOCK},
+            {BLOCK,CARROT,EMPTY,BLOCK,BLOCK,EMPTY,BLOCK,BLOCK,BLOCK,EMPTY,BLOCK,BLOCK,BLOCK,EMPTY,BLOCK,BLOCK,CARROT,BLOCK},
+            {BLOCK,BLOCK,BLOCK,BLOCK,BLOCK,BLOCK,BLOCK,BLOCK,BLOCK,BLOCK,BLOCK,BLOCK,BLOCK,BLOCK,BLOCK,BLOCK,BLOCK,BLOCK}
     };
+
 
     private Rabbit rabbit;
 
@@ -43,7 +49,7 @@ public class Level1 extends GamePanel{
         this.setLayout(null);
         rabbit = new Rabbit( 12, 8, TILE_SIZE);
         setupKeyAdapter();
-
+        this.totalCarrot = 7;
 
     }
     private void setupKeyAdapter(){
@@ -133,6 +139,10 @@ public class Level1 extends GamePanel{
                 graphics.drawRect(c * TILE_SIZE, r * TILE_SIZE, TILE_SIZE, TILE_SIZE);
 
             }
+            graphics.setColor(Color.ORANGE);
+            graphics.fillOval(750 , 30, 20, 20);
+            graphics.setColor(Color.BLACK);
+            graphics.drawString(this.carrotCount + "/" + this.totalCarrot, 790, 40);
         }
 
 
@@ -145,6 +155,16 @@ public class Level1 extends GamePanel{
 //        }
         rabbit.paint(graphics);
 
+    }
+
+    public void colectCarrot (int r, int c){
+        if(map[r][c] == CARROT){
+            map[r][c] = EMPTY;
+            carrotCount++;
+        }
+    }
+    public boolean nextLevel(){
+        return carrotCount == totalCarrot;
     }
 
     }
